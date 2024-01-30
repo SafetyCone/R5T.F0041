1d9e34a5ff6084261f839438f4a385290f569db0
davidcoats <david@getsafetycone.com>
2024-01-29 18:32:53 -0800

    Interim changes.

--- source/R5T.F0041/Code/Functionality/IDirectoryPathOperator.cs
+++ source/R5T.F0041/Code/Functionality/IDirectoryPathOperator.cs
@@ -14,13 +14,13 @@ namespace R5T.F0041
         {
 			var ownerDirectoryName = Instances.RepositoryDirectoryNameOperator.GetRepositoryOwnerDirectoryName(repositoryOwnerName);
 
-			var localOwnerRepositoryDirectoryPath = Instances.PathOperator.GetDirectoryPath(
+			var localOwnerRepositoryDirectoryPath = Instances.PathOperator.Get_DirectoryPath(
 				Instances.DirectoryPaths.GitHubRepositoriesDirectory,
 				ownerDirectoryName);
 
 			var repositoryDirectoryName = Instances.RepositoryDirectoryNameOperator.GetRepositoryDirectoryName(repositoryName);
 
-			var localRepositoryDirectoryPath = Instances.PathOperator.GetDirectoryPath(
+			var localRepositoryDirectoryPath = Instances.PathOperator.Get_DirectoryPath(
 				localOwnerRepositoryDirectoryPath,
 				repositoryDirectoryName);
 
